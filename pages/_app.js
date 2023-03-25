e63ff13d0c74f877798b81b6a24ff1122cfd4b70
Alane38 <52476035+Alane38@users.noreply.github.com>
2023-03-25 22:52:01 +0100

    Clicked items auto id work & can be deleted

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+// import Font Awesome CSS
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Tell Font Awesome to skip adding the CSS automatically 
+// since it's already imported above
+config.autoAddCss = false; 
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
